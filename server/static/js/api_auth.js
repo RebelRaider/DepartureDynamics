215e34bb8f5804db61bef5c409511fb31ac6c3f5
RebelRaider <leonid.chesnikov@gmail.com>
2023-12-17 23:07:26 +0300

    upgate js

--- server/static/js/api_auth.js
+++ server/static/js/api_auth.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.log("error");
             } else {
                 localStorage.setItem('token', data.token);
-                window.location.replace('yakutia5.html');
+                window.location.replace('/lk');
             }
         })
         .catch(error => {
